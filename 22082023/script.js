f08f9acddcdec8753eac54680a95621d518b391b
Julia Teixeira Barbosa <juliateixeira22@hotmail.com>
2023-08-24 21:20:43 -0300

    atividade

--- 22082023/script.js
+++ 22082023/script.js
@@ -60,7 +60,7 @@ function CalcularPeso() {
     if (altura == '') {
         resp3.innerHTML = 'Preencha os campos!'
     } else {
-        resp3.innerHTML = 'Seu peso ideal é: ' + ((altura*altura)*25).toFixed(2) + 'kg'
+        resp3.innerHTML = 'Seu peso ideal é: ' + ((altura * altura) * 25).toFixed(2) + 'kg'
 
     }
 }
@@ -126,9 +126,9 @@ function CalcularCombustivel() {
     var gasolina = document.getElementById('gasolina').value
     var alcool = document.getElementById('alcool').value
     var resp6 = document.getElementById('resp6')
-    var gastoGasolina = (Number(distancia) * gasolina)/11
-    var gastoAlcool = (Number(distancia) * alcool)/9
-    
+    var gastoGasolina = (Number(distancia) * gasolina) / 11
+    var gastoAlcool = (Number(distancia) * alcool) / 9
+
     if (distancia == '' || gasolina == '' || alcool == '') {
         resp6.innerHTML = 'Preencha os campos!'
     } else {
@@ -151,10 +151,10 @@ function limparCombustivel() {
 
 function CalcularPreco() {
     var fabrica = document.getElementById('fabrica').value
-    var impostos = (Number(fabrica) + (Number(fabrica)*0.45))
-    var distribuidor = impostos + impostos*0.28
+    var impostos = (Number(fabrica) + (Number(fabrica) * 0.45))
+    var distribuidor = impostos + impostos * 0.28
     var resp7 = document.getElementById('resp7')
-    
+
     if (fabrica == '') {
         resp7.innerHTML = 'Preencha os campos!'
     } else {
@@ -167,4 +167,121 @@ function limparPreco() {
     fabrica.value = ""
     resp7.innerHTML = 'Custo do consumidor: '
 
+}
+
+
+
+
+
+function Salvar() {
+    var selectCorFundo = document.querySelector('#CorFundo').value
+    var selectCorFundoCard = document.querySelector('#CorFundoCard').value
+    var selectCorFonte = document.querySelector('#CorFonte').value
+    var selectCorBotao1 = document.querySelector('#CorBotao1').value
+    var selectCorBotao2 = document.querySelector('#CorBotao2').value
+    var selectFonte = document.querySelector('#fonte').value
+
+    var corFundoFonte = document.querySelector('.page')
+
+    const linha = document.querySelectorAll(".linhaFlex");
+    linha.forEach((item) => {
+        if (selectFonte == 'times') {
+            item.style.fontFamily = 'Times New Roman, Times, serif'
+        }
+        else if (selectFonte == 'arial') {
+            item.style.fontFamily = 'Arial'
+        }
+        else {
+            item.style.fontFamily = 'Arial'
+        }
+    });
+
+
+
+
+    if (selectCorFundo == 'purple') {
+        corFundoFonte.style.backgroundColor = 'purple'
+    }
+    else if (selectCorFundo == 'blue') {
+        corFundoFonte.style.backgroundColor = 'blue'
+    }
+    else if (selectCorFundo == 'black') {
+        corFundoFonte.style.backgroundColor = 'black'
+    }
+    else {
+        corFundoFonte.style.backgroundColor = '#f9f9f9'
+    }
+
+    if (selectCorFonte == 'purple') {
+        corFundoFonte.style.color = 'purple'
+    }
+    else if (selectCorFonte == 'blue') {
+        corFundoFonte.style.color = 'blue'
+    }
+    else if (selectCorFonte == 'black') {
+        corFundoFonte.style.color = 'black'
+    }
+    else {
+        corFundoFonte.style.color = 'black'
+    }
+
+    const CorFundoCard = document.querySelectorAll(".container");
+    CorFundoCard.forEach((item) => {
+        if (selectCorFundoCard == 'purple') {
+            item.style.backgroundColor = 'purple'
+        }
+        else if (selectCorFundoCard == 'blue') {
+            item.style.backgroundColor = 'blue'
+        }
+        else if (selectCorFundoCard == 'black') {
+            item.style.backgroundColor = 'black'
+        }
+        else {
+            item.style.backgroundColor = 'white'
+        }
+    });
+
+    const CorBotao1 = document.querySelectorAll(".botao");
+    CorBotao1.forEach((item) => {
+        if (selectCorBotao1 == 'purple') {
+            item.style.backgroundColor = 'purple'
+        }
+        else if (selectCorBotao1 == 'blue') {
+            item.style.backgroundColor = 'blue'
+        }
+        else if (selectCorBotao1 == 'black') {
+            item.style.backgroundColor = 'black'
+        }
+        else {
+            item.style.backgroundColor = 'rgb(228, 62, 190)'
+        }
+    });
+
+    const CorBotao2 = document.querySelectorAll(".limpar");
+    CorBotao2.forEach((item) => {
+        if (selectCorBotao2 == 'purple') {
+            item.style.backgroundColor = 'purple'
+        }
+        else if (selectCorBotao2 == 'blue') {
+            item.style.backgroundColor = 'blue'
+        }
+        else if (selectCorBotao2 == 'black') {
+            item.style.backgroundColor = 'black'
+        }
+        else {
+            item.style.backgroundColor = 'rgb(238, 170, 222)'
+        }
+    });
+}
+
+function limparConfig() {
+    document.querySelector('#CorFundo').value = 'white'
+    document.querySelector('#CorFundoCard').value = 'white'
+    document.querySelector('#CorFonte').value = 'white'
+    document.querySelector('#CorBotao1').value = 'white'
+    document.querySelector('#CorBotao2').value = 'white'
+    document.querySelector('#fonte').value = 'padrao'
+
+    Salvar()
+    
 }
